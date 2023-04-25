bb10bf697777da2e32cc6a839e88aa8a19cdedec
Tom <mostitom11@gmail.com>
2023-04-25 15:57:53 +0200

    Supprime crud-test

--- src/crud-test/dto/update-crud-test.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCrudTestDto } from './create-crud-test.dto';
-
-export class UpdateCrudTestDto extends PartialType(CreateCrudTestDto) {}
